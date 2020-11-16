7c22e065074b8a5012da23d8323da6b17812118a
Rafael Bacungan <rbb52775@gmail.com>
2020-11-16 11:29:17 +0800

    initial project commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+import projectDetails from './components/projects/ProjectDetails';
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn'
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+        </Switch>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
